87b5f4f466d14cc9d46a302f93b9ca0a3ff46cf1
fcu-d0440686 <sha60127@gmail.com>
2016-10-18 20:07:57 +0800

    Update CheckOddEven.java

--- src/CheckOddEven.java
+++ src/CheckOddEven.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class CheckOddEven {
 
+
 	public static void main(String[] args) {
+		
 		Scanner keyboard= new Scanner(System.in);
 		System.out.println("Enter a integer:");
-		
+		int i=keyboard.nextInt();
 		if(i%2==1)
 			System.out.println("The input integer is Odd Number.");
-		else if(i%2==0)
+		else
 			System.out.println("The input integer is Even Number.");
 	}
 
+	
 }
