e6d0ba99ccd05ac9eee5b1c0d5dadea735ece3ff
user <user@6.1.1.1>
2016-10-18 23:39:39 +0800

    hw3

--- src/StringComparator.java
+++ src/StringComparator.java
@@ -5,20 +5,18 @@ public class StringComparator {
 		int flag=1;
 		Scanner keyboard= new Scanner(System.in);
 		while(flag==1){
-		System.out.println("Enter a string 1:");
-		String one=keyboard.next();
-		System.out.println("Enter a string 2:");
-		String two=keyboard.next();
-		if(one.equalsIgnoreCase(two)){
-			System.out.println("The two strings are the same.");
-			flag=0;
-		}
-		else{
-			System.out.println("The two strings are not the same.");
-			flag=1;
-		}
-			
+			System.out.println("Enter a string 1:");
+			String one=keyboard.next();
+			System.out.println("Enter a string 2:");
+			String two=keyboard.next();
+			if(one.equalsIgnoreCase(two)){
+				System.out.println("The two strings are the same.");
+				flag=0;
+			}
+			else{
+				System.out.println("The two strings are not the same.");
+				flag=1;
+			}
 		}
 	}
-
 }
