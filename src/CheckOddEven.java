5dbf9ced29d7e471aefcd612b7fd0cce9104df87
user <user@6.1.1.1>
2016-10-18 23:22:05 +0800

    HW3

--- src/CheckOddEven.java
+++ src/CheckOddEven.java
@@ -5,7 +5,7 @@ public class CheckOddEven {
 	public static void main(String[] args) {
 		Scanner keyboard= new Scanner(System.in);
 		System.out.println("Enter a integer:");
-		
+		int i=keyboard.nextInt();
 		if(i%2==1)
 			System.out.println("The input integer is Odd Number.");
 		else if(i%2==0)
--- src/StringComparator.java
+++ src/StringComparator.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class StringComparator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int flag=1;
 		Scanner keyboard= new Scanner(System.in);
+		while(flag==1){
 		System.out.println("Enter a string 1:");
 		String one=keyboard.next();
 		System.out.println("Enter a string 2:");
 		String two=keyboard.next();
-		//boolean flag = one.equals(two);
-		//System.out.println(flag);
-		if(one.equalsIgnoreCase(two))
+		if(one.equalsIgnoreCase(two)){
 			System.out.println("The two strings are the same.");
-		else
+			flag=0;
+		}
+		else{
 			System.out.println("The two strings are not the same.");
+			flag=1;
+		}
+			
+		}
 	}
 
 }
