5e948203f735454c57309eeb8121906244bb11ad
fcu-d0440686 <sha60127@gmail.com>
2016-10-19 10:13:03 +0800

    Add files via upload

---
+++ OOP-HW2/src/Break.java
@@ -0,0 +1,11 @@
+public class Break {
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i ++) {
+			if(i==2)
+				break;
+			
+			else
+				System.out.println(i);
+		}
+	}
+}
---
+++ OOP-HW2/src/StringComparator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class StringComparator {
+	public static void main(String[] args) {
+		int flag=1;
+		Scanner keyboard= new Scanner(System.in);
+		while(flag==1){
+			System.out.println("Enter a string 1:");
+			String one=keyboard.next();
+			System.out.println("Enter a string 2:");
+			String two=keyboard.next();
+			if(one.equalsIgnoreCase(two)){
+				System.out.println("The two strings are the same.");
+				flag=0;
+			}
+			else{
+				System.out.println("The two strings are not the same.");
+				flag=1;
+			}
+		}
+	}
+}
