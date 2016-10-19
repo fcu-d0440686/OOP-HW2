37587abbec437d72e10b7728cb04cfdd080f5d92
fcu-d0440686 <sha60127@gmail.com>
2016-10-19 10:16:17 +0800

    Add files via upload

--- src/StringComparator.java
+++ src/StringComparator.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
 public class StringComparator {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int flag=1;
 		Scanner keyboard= new Scanner(System.in);
-		System.out.println("Enter a string 1:");
-		String one=keyboard.next();
-		System.out.println("Enter a string 2:");
-		String two=keyboard.next();
-		//boolean flag = one.equals(two);
-		//System.out.println(flag);
-		if(one.equalsIgnoreCase(two))
-			System.out.println("The two strings are the same.");
-		else
-			System.out.println("The two strings are not the same.");
+		while(flag==1){
+			System.out.println("Enter a string 1:");
+			String one=keyboard.next();
+			System.out.println("Enter a string 2:");
+			String two=keyboard.next();
+			if(one.equalsIgnoreCase(two)){
+				System.out.println("The two strings are the same.");
+				flag=0;
+			}
+			else{
+				System.out.println("The two strings are not the same.");
+				flag=1;
+			}
+		}
 	}
-
 }
